acbae30746f2cdc105dcd17f9d77818768a4d324
Devashish Ranjan <devashish.ranjan_cs18@gla.ac.in>
2020-10-16 21:25:19 +0530

    added

--- twitter.py
+++ twitter.py
@@ -92,7 +92,7 @@ def auto():
             print('found it', flush=True)
             print('responding back...', flush=True)
             api.update_status('@' + mention.user.screen_name +
-                              tweet + "You see that ! So, Please wear a mask and stay safe.", mention.id)
+                              tweet + mention.id)
             api.retweet(mention.id)
             api.create_favorite(mention.id)
         else:
